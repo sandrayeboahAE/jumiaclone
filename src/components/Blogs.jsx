93328bc07b04bc538d6c2134003e386be36afe4e
Sandra Yeboah <119595994+sandrayeboahAE@users.noreply.github.com>
2024-12-04 16:53:32 +0000

    designed blog posts

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,66 +1,76 @@
 import { Carousel } from "@mantine/carousel";
 import { Text, Title } from "@mantine/core";
+import { MdDateRange } from "react-icons/md";
+import { RiMessage2Fill } from "react-icons/ri";
 import Image from "next/image";
+import Link from "next/link";
 
 const Blogs = () => {
+  const blogPosts = [
+    {
+      image: "/saloon-image-1.jpeg",
+      date: "December 04, 2024",
+      number: "34",
+      title: "BLOG TITLE",
+      details:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam.",
+    },
+    {
+      image: "/saloon-image-2.jpeg",
+      date: "December 04, 2024",
+      number: "34",
+      title: "BLOG TITLE",
+      details:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam.",
+    },
+    {
+      image: "/saloon-image-3.jpeg",
+      date: "December 04, 2024",
+      number: "34",
+      title: "BLOG TITLE",
+      details:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam.",
+    },
+  ];
+
   return (
     <div className="flex flex-col gap-2 justify-center items-center align-middle w-[80%] m-auto">
-      <div>
-        <Title className="m-5">BLOGS</Title>
-        <Text>Tips For Beauty</Text>
+      <div className="flex flex-col justify-center align-middle items-center">
+        <p className="font-extrabold text-6xl m-5">BLOGS</p>
+        <p className="font-medium text-lg text-orange-900">Tips For Beauty</p>
       </div>
-      <div className="flex flex-row w-[80%] mx-auto my-20 items-center border border-black h-[450px]">
-        <div className="bg-blue-400 w-96 h-96 m-3">
-          <div>
-            <Image
-              src="/saloon-image-1.jpeg"
-              alt="saloon image"
-              width="400"
-              height="200"
-            />
+      <div className="flex flex-row w-[80%] mx-auto my-20 border-0 shadow-md border-black h-[650px]">
+        {blogPosts.map((blogPost) => (
+          <div className="w-96 m-3">
+            <div className="w-full h-[70%]  relative mb-3">
+              <img
+                src={blogPost.image}
+                alt="saloon image"
+                className="w-full h-[100%]"
+              />
+              <div className="bg-white  w-[80%] h-20 absolute bottom-0 left-0">
+                <div className="static">
+                  <Link href={"/blog"}>
+                    <div className="bg-orange-700 w-[90%] h-11 absolute bottom-0 left-0 flex align-middle text-white gap-3 justify-center items-center">
+                      <div className="flex align-middle gap-2 justify-center items-center">
+                        <MdDateRange />
+                        <Text>{blogPost.date}</Text>
+                      </div>
+                      <div className="flex align-middle gap-2 justify-center items-center">
+                        <RiMessage2Fill />
+                        <Text>{blogPost.number}</Text>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="w-full h-40 flex flex-col gap-2">
+              <Title>{blogPost.title}</Title>
+              <Text>{blogPost.details}</Text>
+            </div>
           </div>
-          <div>
-            <Title>BLOG TITLE</Title>
-            <Text>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam.
-            </Text>
-          </div>
-        </div>
-        <div className="bg-blue-400 w-96 h-96 m-3">
-          <div>
-            <Image
-              src="/saloon-image-2.jpeg"
-              alt="saloon image"
-              width="200"
-              height="200"
-            />
-          </div>
-          <div>
-            <Title>BLOG TITLE</Title>
-            <Text>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam.
-            </Text>
-          </div>
-        </div>
-        <div className="bg-blue-400 w-96 h-96 m-3">
-          <div>
-            <Image
-              src="/saloon-image-3.jpeg"
-              alt="saloon image"
-              width="200"
-              height="200"
-            />
-          </div>
-          <div>
-            <Title>BLOG TITLE</Title>
-            <Text>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam.
-            </Text>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
